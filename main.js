1d4e4d43b3a7dfe72af231efabedad1461c4fce5
datmemerboi <shravanssd@gmail.com>
2019-12-20 18:07:07 +0530

    Minor changes

--- main.js
+++ main.js
@@ -1,20 +1,22 @@
-const {app, BrowserWindow, ipcMain} = require('electron');
+const {app, BrowserWindow} = require('electron');
+
 let mainWindow;
+
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width:1920,
-    height:1080,
-    resizable:false,
-    webPreferences:{ nodeIntegration:true }
-  });
-  mainWindow.loadFile('index.html');
-  mainWindow.on('closed', ()=>{
-    mainWindow = null;
-  });
+	mainWindow = new BrowserWindow({
+		width:1920,
+		height:1080,
+		resizable:false,
+		webPreferences:{ nodeIntegration:true }
+	});
+	mainWindow.loadFile('index.html');
+	mainWindow.on('closed', ()=>{
+		mainWindow = null;
+	});
 }
 
 app.on('ready', createWindow);
 app.on('activate', ()=>{
-  if(mainWindow==null)
-    createWindow();
+	if(mainWindow==null)
+		createWindow();
 });
