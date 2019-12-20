66a736fedfc9c85c45b5841185dd1a275fc4f347
datmemerboi <shravanssd@gmail.com>
2019-12-20 17:00:09 +0530

    createDiv fn created

--- src/mongoGet.js
+++ src/mongoGet.js
@@ -1,3 +1,21 @@
+function createDiv(head, body) {
+	var br = document.createElement("BR");
+	br.setAttribute('class',"break-line");
+	var divi = document.createElement("DIV");
+	divi.setAttribute('class', "note");
+	divi.setAttribute('contenteditable', "true");
+	var text = document.createElement("P");
+	text.setAttribute('class', "text");
+	var bottomText = document.createElement("P");
+	bottomText.setAttribute('class', "bottom-text");
+	text.innerHTML = head;
+	bottomText.innerHTML = body;
+	divi.appendChild(text);
+	divi.appendChild(bottomText);
+	document.getElementById('container').appendChild(br);
+	document.getElementById('container').appendChild(divi);
+}
+
 const MongoClient = require('mongodb').MongoClient;
 MongoClient.connect("mongodb://localhost:27017", (err, conn)=>{
 	if(err)
@@ -7,8 +25,8 @@ MongoClient.connect("mongodb://localhost:27017", (err, conn)=>{
 			if(err)
 				throw err;
 			res.forEach( (a)=>{
-				// createDiv(a['Head'], a['Body']);
-				console.log(a['Head'], a['Body'])
+				createDiv(a['Head'], a['Body']);
+				// console.log(a['Head'], a['Body'])
 			});
 		});
 	}
