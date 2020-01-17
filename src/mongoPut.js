45536bf23229ec063dbd60c020d2ad53bd66e625
datmemerboi <shravanssd@gmail.com>
2020-01-17 17:52:33 +0530

    postNote function

--- src/mongoPut.js
+++ src/mongoPut.js
@@ -1,18 +1,21 @@
-var row_title = document.getElementsByName('new-title').innerHTML;
-var row_content = document.getElementsByName('new-content').innerHTML;
-console.log(row_title+"::"+row_content)
+function postNote() {
+	var row_title = $('#new-title').html();
+	var row_content = $('#new-content').html();
+	console.log(row_title+"::"+row_content)
 
-MongoClient.connect("mongodb://localhost:27017", (err, conn)=>{
-	if(err){ throw err; }
-	else{
-		if(row_title!=undefined && row_content!=undefined)
-		{
-			var row = {'Title':row_title, 'Content':row_content};
-			conn.db('Quick').collection('Notes').insertOne(row, (err, res)=>{
-				if(err){ throw err; }
-				console.log("inserted");
-			});
+	MongoClient.connect("mongodb://localhost:27017", (err, conn)=>{
+		if(err){ throw err; }
+		else{
+			if(row_title!=undefined && row_content!=undefined)
+			{
+				var row = {'Title':row_title, 'Content':row_content};
+				conn.db('Quick').collection('Notes').insertOne(row, (err, res)=>{
+					if(err){ throw err; }
+					console.log("inserted");
+				});
+			}
 		}
-	}
-	conn.close();
-});
+		conn.close();
+	});
+	window.location.reload();
+}
