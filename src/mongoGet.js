05003e322d8d6b4f360adebab3bf1e80cc9c0cfb
datmemerboi <shravanssd@gmail.com>
2020-01-23 23:56:23 +0530

    Complete deleteNote()

--- src/mongoGet.js
+++ src/mongoGet.js
@@ -1,9 +1,24 @@
 function deleteNote(child){
 	var parent = child.parentNode.parentNode;
-	document.getElementById('container').removeChild(parent);
+	var ID = child.parentNode.id;
+	var mongodb = require('mongodb');
+	MongoClient.connect("mongodb://localhost:27017", (err, conn)=>{
+		if(err)
+			throw err;
+		else{
+			conn.db('Quick').collection('Notes').deleteOne({_id: new mongodb.ObjectID(ID)}, (err, res)=>{
+				if(err)
+					throw err;
+				else{
+					document.getElementById('container').removeChild(parent);
+				}
+			});
+			
+		}
+	});
 }
 
-function createNote(Title, Content) {
+function createNote(Title, Content, ID) {
 	var padded = document.createElement("DIV");
 	padded.setAttribute('class', "note-padding");
 	
@@ -11,6 +26,7 @@ function createNote(Title, Content) {
 	divi.setAttribute('class', "note");
 	divi.setAttribute('name', "note");
 	divi.setAttribute('contenteditable', "true");
+	divi.setAttribute('id', ID);
 	
 	var text = document.createElement("P");
 	text.setAttribute('class', "text");
@@ -44,7 +60,7 @@ MongoClient.connect("mongodb://localhost:27017", (err, conn)=>{
 			if(err)
 				throw err;
 			res.forEach( (a)=>{
-				createNote(a['Title'], a['Content']);
+				createNote(a['Title'], a['Content'], a['_id']);
 			});
 		});
 	}
