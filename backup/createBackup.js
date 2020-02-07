02e1bc6634d7948e29b58d06756f1f926ac56dd4
datmemerboi <shravanssd@gmail.com>
2020-02-07 23:22:17 +0530

    Tidied var 'now'

--- backup/createBackup.js
+++ backup/createBackup.js
@@ -1,7 +1,10 @@
 let date_ob = new Date();
-var now = String(date_ob.getDate())+"-"+String(parseInt(date_ob.getMonth())+1)+"-"+String(date_ob.getFullYear());
-var query = "mongoexport --db Quick --collection Notes --out backup/Quick_Notes_"+now+".json";
-console.log(query);
+let date = String('0'+date_ob.getDate()).slice(-2);
+let month = String('0'+(parseInt(date_ob.getMonth())+1)).slice(-2)
+let year = String(date_ob.getFullYear());
+let now = date+"-"+month+"-"+year
+let query = "mongoexport --db Quick --collection Notes --out backup/Quick_Notes_"+now+".json";
+console.log(query)
 let { exec } = require("child_process");
 exec(query, (err, res)=>{
 	if(err){ throw err; }
