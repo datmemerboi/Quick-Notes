1bc1af9ff5d2d6e2a4c5c484ef3e80ac8c424153
datmemerboi <shravanssd@gmail.com>
2020-03-06 20:07:06 +0530

    Backup from process.cwd

--- src/createBackup.js
+++ src/createBackup.js
@@ -1,12 +1,17 @@
+// To export Mongo into JSON
+// Run the file from parent directory
+
 let date_ob = new Date();
 let date = String('0'+date_ob.getDate()).slice(-2);
 let month = String('0'+(parseInt(date_ob.getMonth())+1)).slice(-2);
 let year = String(date_ob.getFullYear());
 let now = date+"-"+month+"-"+year
-let query = "mongoexport --db Quick --collection Notes --out ../backup/Quick_Notes_"+now+".json";
-console.log(query)
+
+let query = "mongoexport --db Quick --collection Notes --out "+process.cwd()+"/backup/Quick_Notes_"+now+".json";
+// console.log(query)
+
 let { exec } = require("child_process");
 exec(query, (err, res)=>{
 	if(err){ throw err; }
-	else{ console.log("Data has been exported."); }
+	console.log("Exported")
 });
