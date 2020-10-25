398b2af9e5dcb29a338688aa1ff7ce8f47432a8e
Shravan Dave <34474140+datmemerboi@users.noreply.github.com>
2020-10-25 20:36:03 +0530

    Revert "New Layout & Updated Note type"

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 Quick Notes
 =
-
-## v2.0
-Complete re-construction underway
-
-Here's a sample
-![screencap](https://raw.githubusercontent.com/datmemerboi/Quick-Notes/betterment/Screencaps/Interface%20v2.0.jpeg)
+## Electron.js | MongoDB
 
 A desktop software which works with Electron and data storage in MongoDB.
 
-## v1.0
-### Requirements
-**ElectronJS**, **MongoDB**, **MongoClient** module
+Built on Linux Environment (elementaryOS)
 
-~~Default Mongo connection:`mongodb://localhost:27017` | Database: `Quick` | Collection: `Notes`~~
+### Requirements
+**Electronjs**, **MongoDB**, **MongoClient** module
+
+Default Mongo connection:`mongodb://localhost:27017`
+
+Database: `Quick`
+
+Collection: `Notes`
--- main.js
+++ main.js
@@ -1,103 +1,77 @@
-const electron = require('electron'),
-			app = electron.app,
-			BrowserWindow = electron.BrowserWindow,
-			Menu = electron.Menu,
-			child = require('child_process');
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const Menu = electron.Menu;
+const child = require('child_process')
 
-var mainWindow;
+let mainWindow;
 
 function createWindow() {
 	mainWindow = new BrowserWindow({
-		width: 1920,
-		height: 1080,
-		resizable: false,
-		webPreferences: {
-			nodeIntegration: true
-		}
+		width:1920, height:1080,
+		resizable:false, icon:"./favicon.png",
+		webPreferences:{ nodeIntegration:true }
 	});
 	mainWindow.loadFile('index.html');
-	mainWindow.on('closed', () => {
+	mainWindow.on('closed', ()=>{
 		mainWindow = null;
 	});
 }
 
-app.on('ready', () => {
-	let template = [
+app.on('ready', ()=>{
+	var template = [
 		{
-			label: "Menu",
-			submenu: [
+			label:"Menu",
+			submenu:[
 				{
-					label: "Refresh",
-					role: "reload",
-					accelerator: "F5"
-				},
-				{ type: "separator" },
-				{
-					label: "Inspect",
-					click: () => {
-						mainWindow.webContents.openDevTools()
-					},
-					accelerator: "F12"
+					label:"Refresh",
+					role:"reload"
 				}
 			]
 		},
 		{
-			label: "Export"
-		},
-		// {
-		// 	label: "Backup",
-		// 	submenu: [
-		// 		{
-		// 			label: "Export Now",
-		// 			click: () => {
-		// 				child.exec("node src/createBackup.js", (err, stdout)=>{
-		// 					if(err){throw err;}
-		// 				});
-		// 				}
-		// 		},
-		// 		{
-		// 			type: "separator"
-		// 		},
-		// 		{
-		// 			label: "View Backup files",
-		// 			click: () => {
-		// 				electron.shell.showItemInFolder("./backup/ ");
-		// 			}
-		// 		}
-		// 	]
-		// },
-		{
-			label: "About",
-			submenu: [
+			label:"Backup",
+			submenu:[
 				{
-					label: "datmemerboi",
-					click: () => {
-						electron.shell.openExternal("https://datmemerboi.github.io/")
-					}
+					label:"Export Now",
+					click:()=>{
+						child.exec("node src/createBackup.js", (err, stdout)=>{
+							if(err){throw err;}
+						});
+						}
+				},{
+					type:"separator"
 				},
-				{ type: "separator" },
 				{
-					label: "Github Repo",
-					click: () => {
-						electron.shell.openExternal("https://www.github.com/datmemerboi/Quick-Notes/");
+					label:"View Backup files",
+					click:()=>{
+						electron.shell.showItemInFolder("./backup/ ");
 					}
 				}
 			]
 		},
 		{
-			label: "Quit",
-			role: "quit"
+			label:"Github",
+			click:()=>{
+				electron.shell.openExternal("https://github.com/datmemerboi/");
+			}
+		},
+		{
+			label:"Inspect",
+			click:()=>{mainWindow.webContents.openDevTools()},
+			accelerator:"CmdOrCtrl+Shift+I"
+		},
+		{
+			label:"Quit",
+			role:"quit"
 		}
-	];
-
+	]
 	menuBar = Menu.buildFromTemplate(template);
 	Menu.setApplicationMenu(menuBar)
-
 	createWindow();
 });
-
-app.on('activate', () => {
-	if(mainWindow === null) { createWindow(); }
+app.on('activate', ()=>{
+	if(mainWindow==null)
+		createWindow();
 });
-
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
