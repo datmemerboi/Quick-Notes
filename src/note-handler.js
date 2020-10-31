e5eeea5adf6a59fc7228f9625f7f8d2f78c1cef1
datmemerboi <shravanssd@gmail.com>
2020-10-31 15:23:21 +0530

    Delete note fn

--- src/note-handler.js
+++ src/note-handler.js
@@ -4,9 +4,8 @@ function noteDown() {
     newNoteObj.title = $('#new-note-title').text();
   }
   if($('#new-note-content').text().length) {
-    newNoteObj.content = $('#new-note-content')[0].innerText.replaceAll('\n', '<br/>');
+    newNoteObj.content = $('#new-note-content')[0].textContent;
   }
-
   if(newNoteObj.title || newNoteObj.content) {
     genNextID("Note")
     .then(id => {
@@ -14,7 +13,6 @@ function noteDown() {
       newNoteObj.created_at = parseInt(new Date().getTime()/1000);
       readFromFile()
       .then(data => {
-
         data?.docs ? data.docs.push(newNoteObj) : data.docs = [newNoteObj];
         data.total_docs = data.docs.length;
         data.number_of_notes = data.docs.filter(obj => obj.type === "Note").length;
@@ -33,4 +31,24 @@ function noteDown() {
   else {
     alert("Cannot create empty note.");
   }
+};
+
+function deleteNote(noteID) {
+  readFromFile()
+  .then(data => {
+    let index = data?.docs?.indexOf(data.docs.filter(obj => obj.type === "Note" && obj.id === noteID)[0]);
+    if(index > -1) {
+      data.docs.splice(index, 1);
+      data.total_docs = data.docs.length;
+      data.number_of_notes = data.docs.filter(obj => obj.type === "Note").length;
+      writeIntoFile(data)
+      .then(() => {
+        location.reload();
+      })
+    }
+  })
+  .catch(err => {
+    alert("An error occured! Try again.");
+    console.error(err);
+  });
 };
